ad8a7ce9f7f135cfa4c2c9b9baff512d9e658b25
Suyog0-0 <suyogsigdel000@gmail.com>
2025-02-22 16:24:41 +0545

    Animations.js

--- Source Code/javascript/Animations.js
+++ Source Code/javascript/Animations.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function() {
     document.querySelector('.header-text').classList.add('animate');
 });
 
+
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
